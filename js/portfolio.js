191e98ac1943f8a792c5e86426d4a135b7468944
Hanley Chan <hanleychan@gmail.com>
2016-06-16 15:30:14 -0700

    Added project title and description to overlay

--- js/portfolio.js
+++ js/portfolio.js
@@ -13,15 +13,19 @@ $("ul#projectList li img.projectItem").click(function(event) {
     var imageLocation = $(this).attr("src");
     var imageAltText = $(this).attr("alt");
     var applicationLocation = $(this).next("a").attr("href");
+    var applicationDescription = $(this).next("a").children("p").html() + "<br><br>";
+    var projectTitle = $(this).prev("a").children("h4").html();
     var $applicationLink = $("<a></a>");
-    
+
     $applicationLink.attr("href", applicationLocation);
     $applicationLink.text(applicationLocation);
 
     $image.attr("src", imageLocation);
     $image.attr("alt", imageAltText);
     
-    $caption.html("Application Link: ");
+    $caption.html(projectTitle + ":<br><br>");
+    $caption.append(applicationDescription);
+    $caption.append("Application Link: ");
     $caption.append($applicationLink);
 
     $overlay.show();
